c244bf735a79b0041ed8e088aab05ccb47cade9d
Sergiy Matusevych <sergiy.matusevych@gmail.com>
2020-07-28 16:06:18 -0700

    train model on actual data + add a trained version of the model

--- model/model.py
+++ model/model.py
@@ -1,24 +1,43 @@
+#!/usr/bin/env python3
+
+import random
+import pickle
+
 import torch
 from torch.autograd import Variable
 from torch.nn import functional as F
 
-# x_data = Variable(torch.Tensor([[10.0], [9.0], [3.0], [2.0]]))
-# y_data = Variable(torch.Tensor([[90.0], [80.0], [50.0], [30.0]]))
 
-torch.manual_seed(1)
-input_dim = 1
-output_dim = 1
-data_size = 10
-x_data = torch.randn(100, 1) * data_size
-y_data = torch.FloatTensor(map(lambda x: 1 if x > 0 else 0, x_data))
+_DIM_INPUT = 34  # 14 bytes Ethernet header + 20 bytes IP
+_DIM_OUTPUT = 1  # it's a binary classifier
 
-print x_data, y_data
+
+def _read_file(fname, trim=_DIM_INPUT):
+    with open(fname, "rb") as pkfile:
+        data = pickle.load(pkfile)
+        return [d[:trim] for d in data]
+
+
+def _read_data(dtype=torch.float32):
+
+    data_spam = _read_file("data/nmap.pk")
+    data_ham = _read_file("data/wget.pk")
+
+    x_data = data_spam + data_ham
+    y_data = [[1]] * len(data_spam) + [[0]] * len(data_ham)
+
+    data = list(zip(x_data, y_data))
+    random.shuffle(data)
+
+    return (torch.tensor([d[0] for d in data], dtype=dtype),
+            torch.tensor([d[1] for d in data], dtype=dtype))
 
 
 class LinearRegression(torch.nn.Module):
+
     def __init__(self, input_dim, output_dim):
         super(LinearRegression, self).__init__()
-        self.linear = torch.nn.Linear(input_dim, output_dim)
+        self.linear = torch.nn.Linear(_DIM_INPUT, _DIM_OUTPUT)
 
     def forward(self, x):
         y_pred = self.linear(x)
@@ -26,39 +45,63 @@ class LinearRegression(torch.nn.Module):
 
 
 class LogisticRegression(torch.nn.Module):
+
     def __init__(self, input_dim, output_dim):
         super(LogisticRegression, self).__init__()
-        self.linear = torch.nn.Linear(input_dim, output_dim)
+        self.linear = torch.nn.Linear(_DIM_INPUT, _DIM_OUTPUT)
 
     def forward(self, x):
         y_pred = torch.sigmoid(self.linear(x))
         return y_pred
 
 
-# model = LinearRegression(input_dim, output_dim)
-model = LogisticRegression(input_dim, output_dim)
-# criterion = torch.nn.MSELoss()
-criterion = torch.nn.BCELoss(reduction="sum")
-optimizer = torch.optim.SGD(model.parameters(), lr=0.015)
+def _main():
 
-for epoch in range(1000):
-    model.train()
-    optimizer.zero_grad()
-    # Forward pass
-    y_pred = model(x_data)
-    # Compute Loss
-    loss = criterion(y_pred, y_data)
-    # Backward pass
-    loss.backward()
-    optimizer.step()
-    if epoch % 100 == 0:
-        print('epoch {}, loss {}'.format(epoch, loss.item() / data_size))
+    # x_data = Variable(torch.Tensor([[10.0], [9.0], [3.0], [2.0]]))
+    # y_data )= Variable(torch.Tensor([[90.0], [80.0], [50.0], [30.0]]))
 
-new_x = Variable(torch.Tensor([[-5.0], [4.0]]))
-y_pred = model(new_x)
-print("predicted Y value: ", y_pred.data)
+    # data_size = 10
+    # x_data = torch.randn(100, 1) * data_size
+    # y_data = torch.FloatTensor(map(lambda x: 1 if x > 0 else 0, x_data))
+    # print(x_data, y_data)
 
-input_names = ["actual_input_1"] + ["learned_%d" % i for i in range(2)]
-output_names = ["output1"]
-torch.onnx.export(model, x_data, "1-in-1-out.onnx", verbose=True,
-                  input_names=input_names, output_names=output_names)
+    x_data, y_data = _read_data()
+    data_size = len(x_data)
+    print("Read data: X: %s Y: %s\n" % (x_data.shape, y_data.shape))
+
+    # model = LinearRegression(input_dim, output_dim)
+    model = LogisticRegression(_DIM_INPUT, _DIM_OUTPUT)
+    # criterion = torch.nn.MSELoss()
+    criterion = torch.nn.BCELoss(reduction="sum")
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.015)
+
+    for epoch in range(1000):
+        model.train()
+        optimizer.zero_grad()
+        # Forward pass
+        y_pred = model(x_data)
+        # Compute Loss
+        loss = criterion(y_pred, y_data)
+        # Backward pass
+        loss.backward()
+        optimizer.step()
+        if epoch % 100 == 0:
+            print('epoch {}, loss {}'.format(epoch, loss.item() / data_size))
+
+    # new_x = Variable(torch.Tensor([[-5.0], [4.0]]))
+    test_idx = 10
+    new_x = x_data[test_idx]
+    y_pred = model(new_x)
+    print("predicted Y value: %f actual: %f" % (y_pred.data, y_data[test_idx]))
+
+    input_names = ["actual_input_1"] + ["learned_%d" % i for i in range(2)]
+    output_names = ["output1"]
+
+    torch.onnx.export(
+        model, x_data, "model/lr_34b_v0.onnx", verbose=True,
+        input_names=input_names, output_names=output_names,
+        dynamic_axes={"actual_input_1": {0: "sample"}, "output1": {0: "sample"}})
+
+
+if __name__ == "__main__":
+    _main()
