20fe1a0e672e2aa990e6030738f34aa596b7e660
Sergiy Matusevych <sergiy.matusevych@gmail.com>
2020-07-28 21:08:47 -0700

    a script to run Glow on all ONNX files in the repo

---
+++ glow.sh
@@ -0,0 +1,22 @@
+
+GLOW_HOME=~/devel/glow/build/Debug
+
+for MODEL in model/*.onnx
+do
+    MODEL_BASE=`basename $MODEL .onnx`
+    MODEL_DIR="./out/$MODEL_BASE"
+    mkdir -p "$MODEL_DIR"
+    $GLOW_HOME/bin/model-compiler \
+        --model="$MODEL" \
+        --emit-bundle="$MODEL_DIR" \
+        --backend=CPU \
+        --quantization-calibration=none \
+        --quantization-precision=Int8 \
+        --quantization-precision-bias=Int8 \
+        --quantization-schema=symmetric \
+        --bundle-api=static \
+        --bundle-api-verbose \
+        --relocation-model=pic \
+        --optimize-ir \
+        --dump-ir-after-all-passes > "${MODEL_DIR}/${MODEL_BASE}.ll"
+done
