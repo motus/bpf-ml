182980c9ce275e4c81a1b2604730ec6922a1304b
Sergiy Matusevych <sergiy.matusevych@gmail.com>
2020-07-30 14:23:19 -0700

    XDP on Sergiy's PC

--- xdp/xdp.c
+++ xdp/xdp.c
@@ -12,7 +12,11 @@
 #include <linux/ipv6.h>
 
 #include "xdp.h"
-#define SRC_IP 0xc0a81d10
+
+// #define SRC_IP 0xc0a81d10
+
+// 192.168.1.118
+#define SRC_IP 0xC0A80176
 
 struct bpf_map_def SEC("maps") counter_pass = {
     .type        = BPF_MAP_TYPE_ARRAY,
@@ -55,59 +59,61 @@ int xdp(struct xdp_md *ctx)
     eth = data;
     nh_off = sizeof(*eth);
 
-    if (h_proto == htons(ETH_P_IP))
+    if (h_proto != htons(ETH_P_IP))
     {
-        index = parse_ipv4(data, nh_off, data_end); // get the ipv4 header
-        if (index != 6)                             // check if TCP packet or not
-        {
-            return XDP_PASS;
-        }
-        
-        iph = data + nh_off;
-        nh_off = sizeof(*iph);
-
-        if (data_end < data + (54)) // eth+ipv4+tcp=54
-        {                           // verification
-            return XDP_PASS;
-        }
-
-        // y=w*x+b start
-        #pragma unroll
-        for (u8 i = 14; i < 54; ++i) //14th byte to 54th byte
-        {
-            s8 *byte = data + (i); // don't change this
-            y += (*byte) * w[i];
-        }
-        // y=w*x+b stop
-
-        if (y > 0)
-        {
-            if (iph && ntohl(iph->saddr) == (SRC_IP))
-            {
-                u32 key = 0;
-                u32 *val;
-                val = bpf_map_lookup_elem(&counter_drop, &key);
-                if (val)
-                {
-                    (*val)++;
-                    printk("Dropping IP: %x Count: %d\n", ntohl(iph->saddr), *val);
-                }
-            }
-            return XDP_DROP; // droping spam packets
-        }
-
-        if (iph && ntohl(iph->saddr) == (SRC_IP))
-        {
-            u32 key = 0;
-            u32 *val;
-            val = bpf_map_lookup_elem(&counter_pass, &key);
-            if (val)
-            {
-                (*val)++;
-                printk("Passing IP: %x Count: %d\n", ntohl(iph->saddr), *val);
-            }
-        }
+        return XDP_PASS;
     }
+
+    index = parse_ipv4(data, nh_off, data_end); // get the ipv4 header
+    if (index != 6)                             // check if TCP packet or not
+    {
+        return XDP_PASS;
+    }
+
+    iph = data + nh_off;
+    nh_off = sizeof(*iph);
+
+    if (data_end < data + (54)) // eth+ipv4+tcp=54
+    {                           // verification
+        return XDP_PASS;
+    }
+
+    // pass all packets from hosts that are not in demo
+    if (!(iph && ntohl(iph->saddr) == SRC_IP))
+    {
+        return XDP_PASS;
+    }
+
+    // y=w*x+b start
+    #pragma unroll
+    for (u8 i = 14; i < 54; ++i) //14th byte to 54th byte
+    {
+        s8 *byte = data + (i); // don't change this
+        y += (*byte) * w[i];
+    }
+    // y=w*x+b stop
+
+    u32 key = 0;
+    u32 *val;
+
+    if (y > 0)
+    {
+        val = bpf_map_lookup_elem(&counter_drop, &key);
+        if (val)
+        {
+            (*val)++;
+            printk("Dropping IP: %x Count: %d\n", ntohl(iph->saddr), *val);
+        }
+        return XDP_DROP; // droping spam packets
+    }
+
+    val = bpf_map_lookup_elem(&counter_pass, &key);
+    if (val)
+    {
+        (*val)++;
+        printk("Passing IP: %x Count: %d\n", ntohl(iph->saddr), *val);
+    }
+
     return XDP_PASS; // passing correct packets
 }
 
