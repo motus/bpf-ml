c0b254d0116b48d4a141765979115d7c6fee67b4
Sergiy Matusevych <sergiy.matusevych@gmail.com>
2020-07-30 13:51:35 -0700

    reformat the C code for readability. still use the old weights

--- xdp/xdp.c
+++ xdp/xdp.c
@@ -20,14 +20,15 @@ struct bpf_map_def SEC("maps") counter_pass = {
     .value_size  = sizeof(u32),
     .max_entries = 1,
 };
+
 struct bpf_map_def SEC("maps") counter_drop = {
     .type        = BPF_MAP_TYPE_ARRAY,
     .key_size    = sizeof(u32),
     .value_size  = sizeof(u32),
     .max_entries = 1,
 };
-static int w[40] =
-{
+
+static int w[40] = {
     -46, 87, -96, -50, -67, 16, -41, 32, -44, 76, 85, -9, 72, -55, 68, -47,
     43, 50, 51, -10, -83, -45, 6, -55, -66, 95, 82, 82, -64, -128, 27, -83,
     82, 77, -102, -53, 16, 17, -101, -12
@@ -48,26 +49,27 @@ int xdp(struct xdp_md *ctx)
     struct ethhdr *eth;
     struct iphdr *iph;
 
-    s64 y = b;                                         // start with the bias; no need to clamp y
-    h_proto = parse_eth(data, nh_off, data_end);  // get the eth header
+    s64 y = b;                                   // start with the bias; no need to clamp y
+    h_proto = parse_eth(data, nh_off, data_end); // get the eth header
     eth = data;
     nh_off = sizeof(*eth);
 
     if (h_proto == htons(ETH_P_IP))
     {
-        index = parse_ipv4(data, nh_off, data_end); //get the ipv4 header
-        if(index!=6) //check if TCP packet or not
-            return XDP_PASS;
-        iph = data+nh_off;
-
-        nh_off = sizeof(*iph);
-        
-        if (data_end < data + (54)) // eth+ipv4+tcp=54
-        { //verification
+        index = parse_ipv4(data, nh_off, data_end); // get the ipv4 header
+        if (index != 6)                             // check if TCP packet or not
+        {
             return XDP_PASS;
         }
         
-       
+        iph = data + nh_off;
+        nh_off = sizeof(*iph);
+
+        if (data_end < data + (54)) // eth+ipv4+tcp=54
+        {                           // verification
+            return XDP_PASS;
+        }
+
         // y=w*x+b start
         #pragma unroll
         for (u8 i = 14; i < 54; ++i) //14th byte to 54th byte
@@ -79,27 +81,29 @@ int xdp(struct xdp_md *ctx)
 
         if (y > 0)
         {
-            if(iph && ntohl(iph->saddr) == (SRC_IP))
-            {    
-                u32 key=0;
+            if (iph && ntohl(iph->saddr) == (SRC_IP))
+            {
+                u32 key = 0;
                 u32 *val;
                 val = bpf_map_lookup_elem(&counter_drop, &key);
-                if(val){
+                if (val)
+                {
                     (*val)++;
-                printk("Dropping IP: %x Count: %d\n",ntohl(iph->saddr),*val);
+                    printk("Dropping IP: %x Count: %d\n", ntohl(iph->saddr), *val);
                 }
             }
             return XDP_DROP; // droping spam packets
         }
-    
-        if(iph && ntohl(iph->saddr) == (SRC_IP))
-        {    
-            u32 key=0;
+
+        if (iph && ntohl(iph->saddr) == (SRC_IP))
+        {
+            u32 key = 0;
             u32 *val;
             val = bpf_map_lookup_elem(&counter_pass, &key);
-            if(val){
+            if (val)
+            {
                 (*val)++;
-            printk("Passing IP: %x Count: %d\n",ntohl(iph->saddr),*val);
+                printk("Passing IP: %x Count: %d\n", ntohl(iph->saddr), *val);
             }
         }
     }
